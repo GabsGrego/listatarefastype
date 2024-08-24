c7bf4cc3c2c38244104f5f5d10c5effeff2ec135
GabsGrego <gabrielsgrego@uol.com.br>
2024-08-23 22:30:50 -0300

    Mudanças basicas de texto e fundo no app.tsx

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Salve rapeize, criei um app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#974cec',
     alignItems: 'center',
     justifyContent: 'center',
   },
